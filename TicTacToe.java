893197e98715843f53972b66bf4995b460521c41
Suyash Jain <jain.suyash28@gmail.com>
2020-10-03 13:41:47 +0530

    Added: Input Selector to TicTacToe game is Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,41 @@
+package com.briglelabz.tictactoe;
+
+import java.util.Scanner;
 
 public class TicTacToe {
-	
-	char[] boardArray=new char[10];
-	
-	public void createBoard(){
-		for (int i=1; i<boardArray.length;i++) {
-			boardArray[i] = '0';
+	char[] boardArray = new char[10];
+	String userInput = null;
+	String computerInput = null;
+
+	// Method to Create TicTacToe Board
+	private void createBoard() {
+		for (int i = 1; i < boardArray.length; i++) {
+			boardArray[i] = ' ';
 		}
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Welcome to Tic-Tac-Toe Game !!");
+
+	// Method for Player Character(X/O)
+	public String selectInputChar() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter character X or O ");
+		userInput = sc.next();
+		if (userInput.equals("X") || userInput.equals("x")) {
+			computerInput = "O";
+		} else if (userInput.equals("O") || userInput.equals("o")) {
+			computerInput = "X";
+		}else {
+			System.out.println("Incorrect Input");
+		}
+
+		return computerInput;
 	}
 
+	// Main Program
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic-Tac-Toe Game !!");
+		TicTacToe Player = new TicTacToe();
+		Player.createBoard();
+		String computerLetter = Player.selectInputChar();
+	}
 }
+
