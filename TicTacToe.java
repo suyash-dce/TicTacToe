c44029d4911c56b1da3a422784b88eed0b4e921f
Suyash Jain <jain.suyash28@gmail.com>
2020-10-03 13:02:07 +0530

    Added: Board Create Array to TicTacToe game is Added

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,14 @@
-public class TicTacToe {
 
+public class TicTacToe {
+	
+	char[] boardArray=new char[10];
+	
+	public void createBoard(){
+		for (int i=1; i<boardArray.length;i++) {
+			boardArray[i] = '0';
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Welcome to Tic-Tac-Toe Game !!");
